a1a530f406f6f89b359a15f9b50d62eb6d0a7325
enricoottavoita <angelosanges@gmail.com>
2025-03-26 09:30:51 +0100

    Delete price-api/admin directory

--- price-api/admin/includes/generate_hash.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Script per generare un hash sicuro della password
-$password = 'PolloPollo5555.'; // Sostituisci con la tua password
-$hash = password_hash($password, PASSWORD_DEFAULT);
-echo "Password: $password\n";
-echo "Hash: $hash\n";
-?>
